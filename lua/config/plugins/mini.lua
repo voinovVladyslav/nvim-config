10d98a4b8f248507f789938c1901878d3f417f96
Vladyslav Voinov <voinovvladyslav@gmail.com>
2025-05-17 13:03:53 +0300

    format on save

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -2,6 +2,6 @@ return { {
     'echasnovski/mini.nvim',
     config = function()
         local statusline = require 'mini.statusline'
-        statusline.setup {use_icons = true}
+        statusline.setup { use_icons = true }
     end
 } }
