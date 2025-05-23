839eaa65ed458f9b82262389e58ee8cacd55d971
Vladyslav Voinov <voinovvladyslav@gmail.com>
2025-05-23 21:02:55 +0300

    setup ruff and pyright

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -18,12 +18,51 @@ return {
             },
         },
         config = function()
-            -- local capabilities = require('blink.cmp').get_lsp_capabilities()
-            vim.lsp.enable("lua_ls") -- { capabilities = capabilities })
+            -- lua
+            vim.lsp.enable("lua_ls")
+
+            -- python
+            vim.lsp.config('ruff', {
+                init_options = {
+                    settings = {
+                        -- Ruff language server settings go here
+                    }
+                }
+            })
+
+            vim.lsp.enable('ruff')
+
+            vim.lsp.config(
+                'pyright',
+                {
+                    settings = {
+                        pyright = {
+                            -- Using Ruff's import organizer
+                            disableOrganizeImports = true,
+                        },
+                        python = {
+                            analysis = {
+                                -- Ignore all files for analysis to exclusively use Ruff for linting
+                                ignore = { '*' },
+                            },
+                        },
+                    },
+                }
+            )
+
+
+            vim.lsp.enable('pyright')
+
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
                     if not client then return end
+
+                    if client.name == 'ruff' then
+                        -- Disable hover in favor of Pyright
+                        client.server_capabilities.hoverProvider = false
+                    end
+
 
                     if client:supports_method("textDocument/formatting") then
                         vim.api.nvim_create_autocmd('BufWritePre', {
