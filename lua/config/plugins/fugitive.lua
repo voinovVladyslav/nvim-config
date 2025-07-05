686f764b38e5e5731c7b9332d126ee2d1fb8409b
Vladyslav Voinov <voinovvladyslav@gmail.com>
2025-07-05 07:31:24 +0300

    ref: move keymaps to config function

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -1,5 +1,6 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
 return { {
     "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    end
 } }
