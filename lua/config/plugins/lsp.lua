8fe1fc946eb2881b6fd99c99a03b2c4973fe0a4e
Vladyslav Voinov <voinovvladyslav@gmail.com>
2025-11-22 14:20:59 +0200

    feat: setup go lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -112,7 +112,29 @@ return {
                     },
                 }
             })
+            -- rust
             vim.lsp.enable('rust_analyzer')
+
+            -- go
+            vim.lsp.enable('gopls')
+            local lspconfig = require("lspconfig")
+
+            lspconfig.gopls.setup({
+              cmd = { "gopls" },
+              filetypes = { "go", "gomod", "gowork", "gotmpl" },
+              root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+
+              settings = {
+                gopls = {
+                  gofumpt = true,
+                  staticcheck = true,
+                  analyses = {
+                    unusedparams = true,
+                    shadow = true,
+                  },
+                },
+              },
+            })
 
 
             vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end)
