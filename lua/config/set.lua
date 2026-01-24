c4541079fa1ef3eeeed17a56dd9f6f47f1bf85b7
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-01-24 19:33:11 +0100

    ref: remove node_modules from netrw ignore

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -32,7 +32,7 @@ vim.opt.colorcolumn = "81"
 
 vim.g.mapleader = " "
 
-vim.g.netrw_list_hide = "*.pyc,__pycache__,.tox,.git/,node_modules"
+vim.g.netrw_list_hide = "*.pyc,__pycache__,.git/"
 vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
 
 vim.diagnostic.config({ virtual_text = true })
