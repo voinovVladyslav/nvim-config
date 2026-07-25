5ccecfb2ec2d596689caa42fd0230a5cb822c5cb
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-07-25 20:40:36 +0200

    refactor(config): merge blink.cmp configs into one

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ require("config.autocmd")
 
 require("config.lazy")
 
+
--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -10,7 +10,18 @@ return {
             appearance = {
                 nerd_font_variant = 'mono'
             },
-            completion = { documentation = { auto_show = false } },
+            completion = {
+                documentation = { auto_show = false },
+                menu = {
+                    draw = {
+                        columns = {
+                            { 'label',      'label_description', gap = 1 },
+                            { 'kind_icon',  'kind',              gap = 1 },
+                        },
+                    },
+                },
+
+            },
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
             },
